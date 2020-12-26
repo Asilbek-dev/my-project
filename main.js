eb8a1955c93bc6df3b5e787759538790626b3ab4
Asilbek <ergashevasilbek41@gmail.com>
2020-12-26 19:55:09 +0500

    menu scroll and card__block box added

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-function foo() {
+function off() {
     var a = document.getElementById("on");
     var b = document.getElementById("off");
+    var c = document.getElementById("card__tv");
+    var d = document.getElementById("card__theater");
     a.style.backgroundColor = "#ffffff";
     b.style.backgroundColor = "#032541";
+    c.style.display = "none";
+    d.style.display = "block";
 }
 function onn() {
     var a = document.getElementById("on");
     var b = document.getElementById("off");
+    var c = document.getElementById("card__tv");
+    var d = document.getElementById("card__theater");
     b.style.backgroundColor = "#ffffff";
     a.style.backgroundColor = "#032541";
+    c.style.display = "block";
+    d.style.display = "none";
 }
-function threeDot() {
+function threeDot1() {
     var x = document.getElementById("box");
-    var y = document.getElementById("box2");
+    var y = document.getElementById("box1");
 
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -24,4 +32,295 @@ function threeDot() {
     } else {
         y.style.display = "none";
     }
+}
+function threeDot2() {
+    var x = document.getElementById("box2");
+    var y = document.getElementById("box3");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot3() {
+    var x = document.getElementById("box4");
+    var y = document.getElementById("box5");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot4() {
+    var x = document.getElementById("box6");
+    var y = document.getElementById("box7");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot5() {
+    var x = document.getElementById("box8");
+    var y = document.getElementById("box9");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot6() {
+    var x = document.getElementById("box10");
+    var y = document.getElementById("box11");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot7() {
+    var x = document.getElementById("box12");
+    var y = document.getElementById("box13");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot8() {
+    var x = document.getElementById("box14");
+    var y = document.getElementById("box15");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot9() {
+    var x = document.getElementById("box16");
+    var y = document.getElementById("box17");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot10() {
+    var x = document.getElementById("box18");
+    var y = document.getElementById("box19");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot11() {
+    var x = document.getElementById("box20");
+    var y = document.getElementById("box21");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot12() {
+    var x = document.getElementById("box22");
+    var y = document.getElementById("box23");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot13() {
+    var x = document.getElementById("box24");
+    var y = document.getElementById("box25");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot14() {
+    var x = document.getElementById("box26");
+    var y = document.getElementById("box27");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot15() {
+    var x = document.getElementById("box28");
+    var y = document.getElementById("box29");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot16() {
+    var x = document.getElementById("box30");
+    var y = document.getElementById("box31");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot17() {
+    var x = document.getElementById("box32");
+    var y = document.getElementById("box33");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot18() {
+    var x = document.getElementById("box34");
+    var y = document.getElementById("box35");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot19() {
+    var x = document.getElementById("box36");
+    var y = document.getElementById("box37");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function threeDot() {
+    var x = document.getElementById("box");
+    var y = document.getElementById("box1");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    }
+}
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-70px";
+    }
+    prevScrollpos = currentScrollPos;
 }
